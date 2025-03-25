8650806ff5a9df6ad0c218460f72b932c7d8e254
Maxim Yuzviak <shaylushaytop@gmail.com>
2025-03-25 16:38:15 +0100

    done all HW

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let text1 = "daospid";
+let text2 = "asodpsospd";
+if (text1 && text2){
+  console.log("Обидва поля заповнені");
+} else {
+  console.log("Не всі поля заповнені");
+}
+let number1 = 3;
+let number2 = 4;
+if ((number1+number2)>10){
+  console.log("Сума більша за 10");
+} else {
+  console.log("Сума менша або дорівнює 10");
+}
+let textJavaScript = "PythonJavaCSharpRubyPHPJavaScriptGoSwiftKotlinHtmlCssTypeScriptPerlScalaDartHaskell"
+if (textJavaScript.includes("JavaScript")){
+  console.log("Текст містить слово JavaScript");
+} else {
+  console.log("Текст не містить слово JavaScript");
+}
+let numberEx4 = 16;
+if (10 && numberEx4 < 20){
+  console.log("Число входить в діапазон від 10 до 20");
+} else {
+  console.log("Число не входить в діапазон від 10 до 20");
+}
+let userName = "wyft";
+let email = "sdfkl@dhfjfhs";
+let password = "4985725";
+if (3 < userName.length && email.includes("@") && password.length > 6){
+  console.log("Перенаправлення на іншу сторінку");
+} else {
+  console.log("Помилка: неправильне заповнення");
+}
